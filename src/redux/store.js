45d6dff90356f95d0d8b0c4cb643102a6ab6ef2a
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-03-30 16:09:13 -0400

    Added redux-persist to persist cart into session storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 
 // This object is create to hold all middleware calls
 // the applyMiddleware() function can actually take
@@ -11,6 +12,8 @@ const middlewareObjects = [logger];
 // Using ...middlewareObjects spreads all objects as 
 // individual parameters in the function. Otherwise,
 // we could just list them out.
-const store = createStore(rootReducer, applyMiddleware(...middlewareObjects));
+export const store = createStore(rootReducer, applyMiddleware(...middlewareObjects));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
