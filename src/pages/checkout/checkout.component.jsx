f4f497fe360e7de85d51b19d5fb64bff3df8a3c8
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-03-31 23:03:40 -0400

    More responsive design changes to SignUp and Checkout pages.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,32 +10,33 @@ import CheckOutItem from '../../components/checkout-item/checkout-item.component
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckOutPage = ({cartItems, cartTotalCost}) => (
+    <div className="checkout-outer">
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">Product</div>
+                <div className="header-block">Description</div>
+                <div className="header-block">Quantity</div>
+                <div className="header-block">Price</div>
+                <div className="header-block">Remove</div>
+            </div>
+            
+            {
+                cartItems.length ?
+                    cartItems.map(cartItem => (
+                        <CheckOutItem key={cartItem.id} cartItem={cartItem} />
+                    ))
+                :
+                    <span className="empty-message">Your cart is empty.</span>
+            }
 
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">Product</div>
-            <div className="header-block">Description</div>
-            <div className="header-block">Quantity</div>
-            <div className="header-block">Price</div>
-            <div className="header-block">Remove</div>
+            <div className="total">Total: ${cartTotalCost}</div>
+            {
+                !cartTotalCost ? null : (
+                    <StripeCheckoutButton price={cartTotalCost} />
+                )
+            }
+            <div className="test-warning">Test Credit Card: 4242 4242 4242 4242<br/>Security Code: 123<br/>Expiration : Any Future Date</div>
         </div>
-        
-        {
-            cartItems.length ?
-                cartItems.map(cartItem => (
-                    <CheckOutItem key={cartItem.id} cartItem={cartItem} />
-                ))
-            :
-                <span className="empty-message">Your cart is empty.</span>
-        }
-
-        <div className="total">Total: ${cartTotalCost}</div>
-        {
-            !cartTotalCost ? null : (
-                <StripeCheckoutButton price={cartTotalCost} />
-            )
-        }
-        <div className="test-warning">Test Credit Card: 4242 4242 4242 4242<br/>Security Code: 123<br/>Expiration : Any Future Date</div>
     </div>
 );
 
